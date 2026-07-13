014b6bc30a8906c06c20aa8a2151448030271910
artem-emelkin <mrbebrarezerv@mail.ru>
2026-07-13 20:41:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 # ===== Конфигурация =====
-TOKEN = "8703228304:AAFeucvEa_loFuKBLAcFLNFM01n_3D8vW7g"
+TOKEN = "8790618971:AAH4QQ29F5b7JUw7PW-EW4S2xTZXlE2R6eY"
 ADMIN_IDS = [8071127858, 711314367]
 
 DB_NAME = "tasks.db"
